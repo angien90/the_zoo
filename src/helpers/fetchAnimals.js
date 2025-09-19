aa899b93f0d2688b5bc21a8fd4bd77e4d6603c98
angien90 <angelicanylander@hotmail.com>
2025-09-19 13:38:51 +0200

    Försök till att lösa animals.json

--- src/helpers/fetchAnimals.js
+++ src/helpers/fetchAnimals.js
@@ -5,10 +5,18 @@ import fetch from "node-fetch";
 
 const url = "https://animals.azurewebsites.net/api/animals";
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
+const fetchAndSaveAnimals = async () => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.json();
     fs.writeFileSync("animals.json", JSON.stringify(data, null, 2));
     console.log("Datan sparad i animals.json ✅");
-  })
-  .catch(err => console.error(err));
+  } catch (err) {
+    console.error("Fel vid hämtning eller skrivning av filen:", err);
+  }
+};
+
+fetchAndSaveAnimals();
